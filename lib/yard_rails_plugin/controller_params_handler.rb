7a50489e2bf6d29967b1644194b23226491d3766
Péter Nagy <peter@antronin.consulting>
2024-04-26 16:49:36 +0200

    Refactor messy codes

--- lib/yard_rails_plugin/controller_params_handler.rb
+++ lib/yard_rails_plugin/controller_params_handler.rb
@@ -3,12 +3,14 @@
 module YARD
   module Rails
     module Plugin
+      # Handles Rails controller parameters
       class ControllerParamsHandler < YARD::Handlers::Ruby::Base
         handles(/params\[(:|')\w+'?\]/)
         def process
           return unless owner.is_a?(YARD::CodeObjects::MethodObject)
-          return unless owner.parent.is_a?(YARD::CodeObjects::ClassObject)
-          return unless owner.parent.name.to_s[/Controller/]
+
+          op = owner.parent
+          return unless op.is_a?(YARD::CodeObjects::ClassObject) || op.name.to_s[/Controller/]
 
           (owner[:params] ||= []) << statement.source.match(/params\[((:|')\w+'?)\]/)[1]
         end
--- lib/yard_rails_plugin/routes.rb
+++ lib/yard_rails_plugin/routes.rb
@@ -1,67 +1,105 @@
 # frozen_string_literal: true
 
+require 'erb'
+
 module YARD
   module Rails
     module Plugin
+      # Handles Rails's route documentations
       class Routes
         def initialize
           puts '[rails-plugin] Analyzing Routes...'
-          ::Rails.application.reload_routes!
-          all_routes = ::Rails.application.routes.routes
-          @routes = all_routes.collect do |route|
+          @routes = load_routes.collect do |route|
+            next if route[:path] =~ '/rails/info/properties|^/assets'
+
             reqs = route.requirements.dup
-            rack_app =  route.app.class.name.to_s =~ /^ActionDispatch::Routing/ ? nil : route.app.inspect
-            constraints = reqs.except(:controller, :action).empty? ? '' : reqs.except(:controller, :action).inspect
-            controller = ''
-            if reqs[:controller]
-              controller = "#{reqs[:controller]}_controller"
-              controller = (controller.split('_').map { |s| s[0].upcase + s[1..] }).join('')
-              controller = (controller.split('/').map { |s| s[0].upcase + s[1..] }).join('::')
-            end
             { name: route.name.to_s,
               verb: route.verb.to_s,
               path: route.path.spec.to_s,
               action: reqs[:action],
-              controller:,
-              rack_app:,
-              constraints: }
+              controller: get_controller(reqs),
+              rack_app: get_rack_app(route),
+              constraints: get_constraints(reqs) }
           end
-          @routes.reject! { |r| r[:path] =~ '/rails/info/properties|^/assets' }
         end
 
         def generate_routes_description_file(filename)
-          File.open(File.join(Dir.pwd, filename), 'w') do |f|
-            f.puts '<h1>Routes</h1><br /><br />'
-            f.puts "<table><tr style='background: #EAF0FF; font-weight: bold; line-height: 28px; text-align: left'><th>&nbsp;&nbsp;Rails Method</th><th>&nbsp;&nbsp;Verb</th><th>&nbsp;&nbsp;Endpoint</th><th>&nbsp;&nbsp;Destionation</th></tr>"
-            i = 0
-            @routes.each do |r|
+          template = %q(
+          <h1>Routes</h1>
+          <table>
+            <tr style='background: #EAF0FF; font-weight: bold; line-height: 28px; text-align: left'>
+              <th>Rails Method</th>
+              <th>Verb</th>
+              <th>Endpoint</th>
+              <th>Destination</th>
+            </tr>
+            <% i = 0;
+             @routes.each do |r|
               odd_or_even = (i.even? ? 'even' : 'odd')
               destination = if r[:rack_app]
-                              "<pre>#{r[:rack_app].inspect} #{r[:constraints]}</pre>"
-                            else
-                              "{#{r[:controller]} #{r[:controller]}}##{r[:action]}  #{r[:constraints]}"
-                            end
+                "<pre>#{r[:rack_app].inspect} #{r[:constraints]}</pre>"
+              else
+                "{#{r[:controller]} #{r[:controller]}}##{r[:action]}  #{r[:constraints]}"
+              end
               endpoint = r[:path].gsub(/(:|\*)\w+/) do |m|
                 "<span style='font-family: monospace; color: green'>#{m}</span>"
               end
-              f.puts "<tr class='#{odd_or_even}'><td>#{r[:name]}</td><td>#{r[:verb]}</td><td>#{endpoint}</td><td>#{destination}</td></tr>"
               i += 1
-            end
-            f.puts '</table>'
+            %>
+            <tr class='#{odd_or_even}'>
+              <td><%= r[:name]%></td>
+              <td><%= r[:verb]%></td>
+              <td><%= endpoint%></td>
+              <td><%= destination%></td>
+            </tr>"
+            <% end %>
+            </table>
+          ').gsub(/^  /, '')
+          erb = ERB.new(template, trim_mode: '%<>')
+
+          File.open(File.join(Dir.pwd, filename), 'w') do |f|
+            f.write erb.result
           end
         end
 
         def enrich_controllers
           @routes.each do |r|
-            if r[:controller] && node = YARD::Registry.resolve(nil, r[:controller], true)
-              (node[:routes] ||= []) << r
-            end
-            if r[:controller] && r[:action] && (node = YARD::Registry.resolve(nil, "#{r[:controller]}##{r[:action]}",
-                                                                              true))
-              (node[:routes] ||= []) << r
-            end
+            next unless r[:controller]
+
+            node = YARD::Registry.resolve(nil, r[:controller], true)
+            (node[:routes] ||= []) << r
+
+            next unless r[:action]
+
+            node = YARD::Registry.resolve(nil, "#{r[:controller]}##{r[:action]}", true)
+            (node[:routes] ||= []) << r
           end
         end
+      end
+
+      private
+
+      def load_routes
+        ::Rails.application.reload_routes!
+        ::Rails.application.routes.routes
+      end
+
+      def get_controller(reqs)
+        return unless reqs[:controller]
+
+        "#{reqs[:controller].upcase.gsub('_', '').gsub('/', '::')}_Controller"
+      end
+
+      def get_constraints(reqs)
+        return unless reqs.except(:controller, :action).present?
+
+        reqs.except(:controller, :action).inspect
+      end
+
+      def get_rack_app(route)
+        return unless route.app.is_a?('ActionDispatch::Routing')
+
+        route.app.inspect
       end
     end
   end
