2f4a319c0eef185e9c26d414e86cb5f5191bce95
Péter Nagy <peter@antronin.consulting>
2024-05-22 15:23:07 +0200

    Fix route processing

--- lib/yard-rails_plugin/routes.rb
+++ lib/yard-rails_plugin/routes.rb
@@ -7,23 +7,24 @@ module YARD
     module Plugin
       # Handles Rails's route documentations
       class Routes
-        # rubocop:disable Metrics/AbcSize
-        # rubocop:disable Metrics/MethodLength
         def initialize
           puts '[rails-plugin] Analyzing Routes...'
           @routes = load_routes.collect do |route|
-            next if route[:path] =~ '/rails/info/properties|^/assets'
+            next if route.path.spec.to_s =~ %r{/rails/*|^/assets|^/cable}
 
             reqs = route.requirements.dup
 
-            return { name: route.name.to_s,
-                     verb: route.verb.to_s,
-                     path: route.path.spec.to_s,
-                     action: reqs[:action],
-                     controller: get_controller(reqs),
-                     rack_app: get_rack_app(route),
-                     constraints: get_constraints(reqs) }
+            next if reqs.blank?
+
+            { name: route.name.to_s,
+              verb: route.verb.to_s,
+              path: route.path.spec.to_s,
+              action: reqs[:action],
+              controller: get_controller(reqs),
+              rack_app: get_rack_app(route),
+              constraints: get_constraints(reqs) }
           end
+          @routes.compact!
         end
 
         def generate_routes_description_file(filename)
@@ -39,73 +40,74 @@ module YARD
                 <th>Endpoint</th>
                 <th>Destination</th>
               </tr>
-              <% i = 0;
-              @routes.each do |r|
-                odd_or_even = (i.even? ? 'even' : 'odd')
-                destination = if r[:rack_app]
-                  "<pre>#{r[:rack_app].inspect} #{r[:constraints]}</pre>"
-                else
-                  "{#{r[:controller]} #{r[:controller]}}##{r[:action]}  #{r[:constraints]}"
-                end
-                endpoint = r[:path].gsub(/(:|\*)\w+/) do |m|
-                  "<span style='font-family: monospace; color: green'>#{m}</span>"
-                end
-                i += 1
+              <% @routes.each_with_index do |r, i|
+                next if r == nil
               %>
-              <tr class='#{odd_or_even}'>
-                <td><%= r[:name]%></td>
-                <td><%= r[:verb]%></td>
-                <td><%= endpoint%></td>
-                <td><%= destination%></td>
-              </tr>"
+              <tr class='<%=(i.even? ? 'even' : 'odd')%>'>
+                <td><%= r[:name] %></td>
+                <td><%= r[:verb] %></td>
+                <td><%= r[:path].gsub(/(:|\*)\w+/) do |m|
+                  "<span style='font-family: monospace; color: green'>#{m}</span>"
+                end %></td>
+                <td><%= r[:rack_app].present? ? "<pre>#{r[:rack_app].inspect} #{r[:constraints]}</pre>" :
+                  "{#{r[:controller]} #{r[:controller]}}##{r[:action]}  #{r[:constraints]}" %></td>
+              </tr>
               <% end %>
               </table>
             </body>
-          </html>').gsub(/^  /, '')
+          </html>).gsub(/^\s+/, '')
           erb = ERB.new(template, trim_mode: '%<>')
 
-          File.write(File.join(Dir.pwd, filename), erb.result)
+          b = binding
+
+          File.write(
+            File.join(Dir.pwd, filename),
+            erb.result(b)
+          )
         end
-        # rubocop:enable Metrics/MethodLength
 
         def enrich_controllers
           @routes.each do |r|
             next unless r[:controller]
 
             node = YARD::Registry.resolve(nil, r[:controller], true)
+            next if node.nil?
+
             (node[:routes] ||= []) << r
 
             next unless r[:action]
 
             node = YARD::Registry.resolve(nil, "#{r[:controller]}##{r[:action]}", true)
+            next if node.nil?
+
             (node[:routes] ||= []) << r
           end
         end
-      end
 
-      private
+        private
 
-      def load_routes
-        ::Rails.application.reload_routes!
-        ::Rails.application.routes.routes
-      end
+        def load_routes
+          ::Rails.application.reload_routes!
+          ::Rails.application.routes.routes
+        end
 
-      def get_controller(reqs)
-        return unless reqs[:controller]
+        def get_controller(reqs)
+          return unless reqs[:controller]
 
-        "#{reqs[:controller].upcase.gsub('_', '').gsub('/', '::')}_Controller"
-      end
+          "#{reqs[:controller].camelize}Controller"
+        end
 
-      def get_constraints(reqs)
-        return unless reqs.except(:controller, :action).present?
+        def get_constraints(reqs)
+          return if reqs.except(:controller, :action).blank?
 
-        reqs.except(:controller, :action).inspect
-      end
+          reqs.except(:controller, :action).inspect
+        end
 
-      def get_rack_app(route)
-        return unless route.app.is_a?('ActionDispatch::Routing')
+        def get_rack_app(route)
+          return unless route.app.is_a?(ActionDispatch::Routing)
 
-        route.app.inspect
+          route.app.inspect
+        end
       end
     end
   end
